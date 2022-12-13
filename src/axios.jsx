65767ca93e0d98b60b5886cd8471332a52609ff4
shipon islam <shiponkhan566@gmail.com>
2022-12-13 15:37:38 +0600

    online url put inside axios call

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 let token = JSON.parse(localStorage.getItem("user")).token;
 const Axios = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://sellbdapi.onrender.com",
   headers: { Authorization: "Bearer " + token },
 });
 export default Axios;
